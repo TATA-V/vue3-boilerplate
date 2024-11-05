3d637cbd2b4ff2246330a99c482604cbb742506b
tatahyeonv <xhrl523@naver.com>
2024-11-05 19:42:45 +0900

    chore

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 import Components from 'unplugin-vue-components/vite';
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -11,11 +10,7 @@ export default defineConfig(({ mode }) => {
     plugins: [
       vue(),
       Components({
-        resolvers: [
-          AntDesignVueResolver({
-            importStyle: false,
-          }),
-        ],
+        dts: true,
       })],
     resolve: {
       alias: {
